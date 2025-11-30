8271233eb5e561b2767866fc615e4cf101242e2d
Arijdev <arij.chowdhuryr@gmail.com>
2025-11-30 14:56:41 +0530

    profile page create

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 
-import Home from "./pages/home/Home";
-import Profile from "./pages/profile/Profile";
+// import Home from "./pages/home/Home";
+import Navbar from "./components/navbar/Navbar";
+import Details from "./pages/profile/Details";
+import Department from "./pages/profile/Department";
+import Query from "./pages/profile/Query";
+import EditProfile from "./pages/profile/EditProfile";
+import Signout from "./pages/profile/Signout";
+
+import ApplyNOC from "./pages/profile/forms/ApplyNOC";
+import ApplyCertificate from "./pages/profile/forms/ApplyCertificate";
+import ApplyQuery from "./pages/profile/forms/ApplyQuery";
 
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+     <Router>
       <Navbar />
-
-      <div className="min-h-screen">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </div>
       
-      <Footer />
-    </BrowserRouter>
+      <Routes>
+        {/* <Route path="/profile" element={<Profile />} /> */}
+        <Route path="/profile/details" element={<Details />} />
+        <Route path="/profile/department" element={<Department />} />
+        <Route path="/profile/query" element={<Query />} />
+        <Route path="/profile/editProfile" element={<EditProfile />} />
+
+        <Route path="/apply/noc" element={<ApplyNOC />} />
+        <Route path="/apply/certificates" element={<ApplyCertificate />} />
+        <Route path="/apply/query" element={<ApplyQuery />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-function Navbar() {
+const Navbar = () => {
+  const [openProfile, setOpenProfile] = useState(false);
+  const [openQuery, setOpenQuery] = useState(false);
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="bg-blue-700 text-white px-6 py-3 shadow-md">
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
 
-export default Navbar
+        {/* Logo */}
+        <Link to="/" className="text-xl font-bold tracking-wide">
+          IT Department
+        </Link>
+
+        {/* Right Menu */}
+        <ul className="flex items-center gap-6">
+
+          {/* Home */}
+          <li>
+            <Link to="/" className="hover:text-gray-200">Home</Link>
+          </li>
+
+          {/* Profile Dropdown */}
+          <li className="relative">
+            <button
+              onClick={() => setOpenProfile(!openProfile)}
+              className="hover:text-gray-200"
+            >
+              Profile ▼
+            </button>
+
+            {openProfile && (
+              <ul className="absolute right-0 mt-2 bg-white text-black rounded-md shadow-lg w-48 py-2">
+                <li className="px-4 py-2 hover:bg-gray-100">
+                  <Link to="/profile/details">Student Details</Link>
+                </li>
+                <li className="px-4 py-2 hover:bg-gray-100">
+                  <Link to="/profile/department">Department</Link>
+                </li>
+                <li className="px-4 py-2 hover:bg-gray-100">
+                  <Link to="/profile/query">Query Addressal</Link>
+                </li>
+                {/* Sign Out */}
+                <li className="px-4 py-2 hover:bg-gray-100 border-t">
+                  <button className="w-full text-left text-red-600">
+                    Sign Out
+                  </button>
+                </li>
+              </ul>
+            )}
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
